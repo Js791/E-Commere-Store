377b3aa9baf28d103a979a2ef9de4a7d5d2f1a86
JS <srejicjemil2@gmail.com>
2021-10-11 17:58:14 -0400

    Completed Flash Messages Milestone1

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -1,12 +1,22 @@
 <?php
 require(__DIR__."/../../partials/nav.php");
 ?>
+
 <h1>Home</h1>
+
 <?php
-if(is_logged_in()){
- echo "Welcome, " . get_user_email();
+if(is_logged_in())
+{
+ echo "Welcome home, " . get_user_email();
+ echo "<pre>" . var_export($session,true) . "</pre>";
 }
-else{
+else
+{
   echo "You're not logged in";
 }
+
+?>
+
+<?php
+require(__DIR__."/../../partials/flash.php");
 ?>
--- public_html/Project/logout.php
+++ public_html/Project/logout.php
@@ -2,4 +2,8 @@
 session_start();
 session_unset();
 session_destroy();
+session_start();
+require(__DIR__ . "/../../lib/functions.php");
+flash("you have been successfully logged out");
 header("Location: login.php");
+//require(__DIR__."/../../partials/flash.php");
